f7d3fec20c3fe4168f6fc18ab3475f8d5f7c8f27
SergeyMaximov <mrknitter7@gmail.com>
2025-05-01 16:13:13 +0300

    HW-02

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -4,7 +4,6 @@ type AddressType = {
 };
 
 export type UserType = {
-    // ПРИДЕТСЯ САМОМУ)
     id: number
     name: string
     age: number
@@ -12,16 +11,15 @@ export type UserType = {
 };
 
 type UserListPropsType = {
-    users: UserType[]; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+    users: UserType[];
 };
 
-export const UserList = (props: UserListPropsType) => {
+export const UserList = ({users}: UserListPropsType) => {
     return (
         <div id={'hw01-users'}>
             <h2>User List:</h2>
-
             <ul>
-                {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+                {users.map((user) => (
                     <li key={user.id} id={`hw01-user-${user.id}`}>
                         <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
                         {user.address.street}, {user.address.city}
